04c8072b2cec514a4e0077eeb21db4354d651593
Sadiq <sadiqjaffer510@gmail.com>
2026-05-21 12:31:22 +0300

    Update db.php

--- src/common/db.php
+++ src/common/db.php
@@ -1,7 +1,11 @@
 <?php
-function getDBConnection(){
-    $pdo = new PDO("mysql:host=localhost;dbname=course","admin","password123");
+function getDBConnection() {
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=course;charset=utf8",
+        "root",
+        ""
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
 }
-?>
